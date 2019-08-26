654f5693f2f2c1e6532603c54c631c4cec30748e
Thanhhff <thanhnguyen200399@gmail.com>
2019-08-26 23:50:56 +0700

    Update Cobb-Douglas.py

--- Week 2/2. Cobb-Douglas/Cobb-Douglas.py
+++ Week 2/2. Cobb-Douglas/Cobb-Douglas.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 
+# Input data
+DataFrame = pd.read_excel('Cobb-Douglas.xls', header=None, skiprows=1)
 
-DataFrame = pd.read_excel('Cobb-Douglas.xls', header=None)
+DataFrame = np.asarray(DataFrame[:])
 
-print(DataFrame)
+n = DataFrame.shape[0]
+
+# X
+L = DataFrame[:, 2].reshape(n, -1)
+K = DataFrame[:, 3].reshape(n, -1)
+
+# y
+P = DataFrame[:, 1].reshape(n, -1)
+
+# Create random b and alpha
+b = np.random.rand()
+alpha = np.random.rand()
+
